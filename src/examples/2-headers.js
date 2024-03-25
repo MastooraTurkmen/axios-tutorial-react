156a59cb9d85b75a09fb8aede7335e8c13bd9742
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 20:57:04 +0430

    set up heards

--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -8,7 +8,14 @@ const Headers = () => {
   const [joke, setJoke] = useState('random dad joke');
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    try {
+      const { data } = await axios(url, {
+        headers: { Accept: 'application/json' }
+      })
+      setJoke(data.joke)
+    } catch (error) {
+      console.log(error.data);
+    }
   };
 
   return (
