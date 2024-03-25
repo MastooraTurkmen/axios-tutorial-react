a6975a53ad201361ab4610d8c983836ae3569fac
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 21:16:25 +0430

    global instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,11 +1,17 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const rep1 = await axios(productsUrl)
+      const rep2 = await axios(randomUserUrl)
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
