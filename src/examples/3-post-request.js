e17e7fe6b8f909bf40d322de70f497d223de3f90
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 21:04:18 +0430

    set up post request

--- src/examples/3-post-request.js
+++ src/examples/3-post-request.js
@@ -8,7 +8,12 @@ const PostRequest = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(name, email);
+    try {
+      const response = await axios.post(url, { name, email })
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   return (
