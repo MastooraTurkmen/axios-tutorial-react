c930afd9b49b1f750ad0f99124b324870a589a96
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 21:28:44 +0430

    custom instance

--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/custome';
+import axios from 'axios';
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const rep1 = await authFetch('/react-store-products')
+      const rep2 = await axios(randomUserUrl)
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
