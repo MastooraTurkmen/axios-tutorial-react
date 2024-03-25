8a895661c838a6249425452d431689fc202bf371
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 19:59:24 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Title from './components/Title';
+import Setup from './examples/1-first-request'
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
