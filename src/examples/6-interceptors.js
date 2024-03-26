78a4b581f38d920969532108a79f96a551942e57
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 08:51:31 +0430

    interceptors

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,10 +1,15 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/interceptors';
 
 const url = 'https://course-api.com/react-store-products';
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const resp = await authFetch('/react-store-products')
+    } catch (error) {
+      
+    }
   };
 
   useEffect(() => {
